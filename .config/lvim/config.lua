2d4c40490d5d16b26ee92233524b77ede40a6f01
Nhan <nguyen6n@uwindsor.ca>
2023-10-31 17:22:19 -0400

    update lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -146,15 +146,9 @@ lvim.plugins = {
     end,
   },
   {
-    -- "karb94/neoscroll.nvim",
-    -- config = function()
-    --   require('neoscroll').setup()
-    -- end
     'declancm/cinnamon.nvim',
     config = function()
       require('cinnamon').setup({
-        -- extra_keymaps = true,
-        -- extended_keymaps = true,
       })
     end
   },
